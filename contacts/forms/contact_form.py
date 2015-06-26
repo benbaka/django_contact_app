f73359d61ad21976027a61f3ad244c72738f1bbd
Benjamin Baka <baka@mpedigree.net>
2015-06-26 16:51:51 +0000

    Ben: Added a folder to house forms and a contact form

---
+++ contacts/forms/contact_form.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label="Name:", required=True)
+    age = forms.IntegerField(label="Age: ", required=True)
+
+    def __init__(self, initial={}):
+        super(forms.Form, self).__init__(initial={})
