16b5a7290938e9b49b31589fd075542a3d1c4690
Benjamin Baka <baka@mpedigree.net>
2015-06-26 15:31:37 +0000

    Ben: Added a test to create a view for the new contacts page

--- contacts/tests/views/test_contact_create_view.py
+++ contacts/tests/views/test_contact_create_view.py
@@ -6,8 +6,8 @@ class ContactViewTest(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_should_return_new_contact_form(self):
-        response = self.client.get("/contact/new")
+    def test_if_create_contact_url_exist(self):
+        response = self.client.get("/contacts/new")
         self.assertEquals(response.status_code, 200)
 
 
