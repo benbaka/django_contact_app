36769967f57ae42bd89f7ea636bfa9878322215b
Benjamin Baka <baka@mpedigree.net>
2015-06-26 17:03:58 +0000

    Ben: Test if form url exists

--- contacts/tests/views/test_contact_create_view.py
+++ contacts/tests/views/test_contact_create_view.py
@@ -6,7 +6,7 @@ class ContactViewTest(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_if_create_contact_url_exist(self):
+    def test_if_new_contact_url_exist(self):
         response = self.client.get("/contacts/new")
         self.assertEquals(response.status_code, 200)
 
