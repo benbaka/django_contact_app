d8d3a42534df737a475b4d6bbc88d9c8e2d37874
Benjamin Baka <baka@mpedigree.net>
2015-08-15 21:25:04 +0000

    Ben: removed semi-colon from label names and changed the style of subclassing a form

--- contacts/forms/contact_form.py
+++ contacts/forms/contact_form.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label="Name:", required=True)
-    age = forms.IntegerField(label="Age: ", required=True)
+    name = forms.CharField(label="Name", required=True)
+    age = forms.IntegerField(label="Age", required=True)
 
-    def __init__(self, initial={}):
-        super(forms.Form, self).__init__(initial={})
+    def __init__(self, *args, **kwargs):
+        super(forms.Form, self).__init__(*args, **kwargs)
