3fee089438af74d255c34f7f71c0cc2e7c7dd64e
Benjamin Baka <baka@mpedigree.net>
2015-09-05 20:36:39 +0000

    Ben: add public field to contact form

--- contacts/forms/contact_form.py
+++ contacts/forms/contact_form.py
@@ -4,6 +4,7 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(label="Name", required=True)
     age = forms.IntegerField(label="Age", required=True)
+    public = forms.BooleanField(label="Public" , required=False)
 
     def __init__(self, *args, **kwargs):
         super(forms.Form, self).__init__(*args, **kwargs)
