8bf0a8bbc7cd4a212a51d8fda6c144e593cf08ef
Benjamin Baka <baka@mpedigree.net>
2015-09-26 14:44:21 +0000

    Ben: category form

--- contacts/forms/categoryForm.py
+++ contacts/forms/categoryForm.py
@@ -1,1 +1,10 @@
-__author__ = 'ben'
+from contacts.models.category import Category
+from django.forms.models import ModelForm
+
+
+class CategoryForm(ModelForm):
+
+    class Meta:
+        model = Category
+        fields = ['name', 'description']
+
