9efb42e4a4e4bbe8207de16c90680dddb7334d55
Benjamin Baka <baka@mpedigree.net>
2015-09-27 08:33:33 +0000

    Ben: added the method to return a url that links to the show. changed the datetimefield to provide a current time when being saved for the first time

--- contacts/models/category.py
+++ contacts/models/category.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.fields import CharField
 
@@ -5,8 +6,11 @@ from django.db.models.fields import CharField
 class Category(models.Model):
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=40)
-    date_created = models.DateTimeField(auto_created=True)
+    date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('category-detail', args=[str(self.id)])
 
     class Meta:
         app_label = "contacts"
