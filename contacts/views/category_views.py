a78a4a88f86f18898c6722be2456edf0792cc722
Benjamin Baka <baka@mpedigree.net>
2015-09-27 08:36:25 +0000

    Ben: create create and detail subclasses

--- contacts/views/category_views.py
+++ contacts/views/category_views.py
@@ -1,1 +1,14 @@
-__author__ = 'ben'
+from contacts.models.category import Category
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+
+
+class CategoryCreateView(CreateView):
+    template_name = "categories/category_form.html"
+    model = Category
+    fields = ['name', 'description']
+
+
+class CategoryDetailView(DetailView):
+    template_name = "categories/category_detail.html"
+    model = Category
