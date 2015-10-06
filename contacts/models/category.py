acf0f8e8362c91285711d65358c0f9d1d9bb7e6f
Benjamin Baka <baka@mpedigree.net>
2015-10-06 15:35:23 +0000

    Ben: Add relationship to category creator

--- contacts/models/category.py
+++ contacts/models/category.py
@@ -1,3 +1,4 @@
+from contacts.models.user_profile import UserProfile
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.fields import CharField
@@ -8,6 +9,7 @@ class Category(models.Model):
     description = models.CharField(max_length=40)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(UserProfile, null=True)
 
     def get_absolute_url(self):
         return reverse('category-detail', args=[str(self.id)])
