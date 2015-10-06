4b1a18b2bb226f18e7c0e9a2c17ac22000bde308
Benjamin Baka <baka@mpedigree.net>
2015-10-06 16:18:04 +0000

    Ben: pass current_user to __init__

--- contacts/forms/contact_form.py
+++ contacts/forms/contact_form.py
@@ -1,10 +1,17 @@
+from contacts.models.category import Category
 from django import forms
 
 
 class ContactForm(forms.Form):
     name = forms.CharField(label="Name", required=True)
     age = forms.IntegerField(label="Age", required=True)
-    public = forms.BooleanField(label="Public" , required=False)
+    public = forms.BooleanField(label="Public", required=False)
+    category = forms.ChoiceField(label="Category", required=True)
 
-    def __init__(self, *args, **kwargs):
+    def _populate_category_field(self):
+        self.fields['category'].choices = [(category.id, category.name) for category in  Category.objects.filter(owner=self.user_profile)]
+
+    def __init__(self, current_user=None,  *args, **kwargs):
         super(forms.Form, self).__init__(*args, **kwargs)
+        self.user_profile = current_user.userprofile_set.filter()[0]
+        self._populate_category_field()
