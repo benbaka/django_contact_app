d4e1b6659c16119213357c96ae55c9e43032ce07
Benjamin Baka <baka@mpedigree.net>
2015-10-06 15:37:35 +0000

    Ben: test should ensure that category link to contact exists

--- contacts/tests/models/test_contact_model.py
+++ contacts/tests/models/test_contact_model.py
@@ -1,3 +1,4 @@
+from contacts.models.category import Category
 from django.test import TestCase
 from contacts.models import Contact
 
@@ -18,3 +19,19 @@ class ContactTestCase(TestCase):
         contact.save()
         self.assertTrue(Contact.objects.count(), 1)
 
+    
+    def test_should_save_a_contact_belonging_to_a_category(self):
+        category = Category(name="friend")
+        category.save()
+
+        self.assertEqual(1, len(Category.objects.filter()))
+        contact = Contact()
+        contact.name = "ben"
+        contact.email_address = "brainychip.gmail.com"
+        contact.category = category
+        contact.save()
+
+        self.assertEqual(contact.category, category)
+
+
+        
