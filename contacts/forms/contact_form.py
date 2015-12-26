b1a9b80e6727060b853a12967894c75f1ce54d99
Benjamin Baka <baka@mpedigree.net>
2015-12-26 16:52:26 +0000

    Ben: because categories is optional it can not be required thus change in form. Also changed from default parameters when initializing form

--- contacts/forms/contact_form.py
+++ contacts/forms/contact_form.py
@@ -6,12 +6,12 @@ class ContactForm(forms.Form):
     name = forms.CharField(label="Name", required=True)
     age = forms.IntegerField(label="Age", required=True)
     public = forms.BooleanField(label="Public", required=False)
-    category = forms.ChoiceField(label="Category", required=True)
+    category = forms.ChoiceField(label="Category", required=False)
 
     def _populate_category_field(self):
         self.fields['category'].choices = [(category.id, category.name) for category in  Category.objects.filter(owner=self.user_profile)]
 
-    def __init__(self, current_user=None,  *args, **kwargs):
+    def __init__(self, current_user,  *args, **kwargs):
         super(forms.Form, self).__init__(*args, **kwargs)
         self.user_profile = current_user.userprofile_set.filter()[0]
         self._populate_category_field()
