35be6c9e13bb5ef5ab3fa5eb919d70280577b755
Benjamin Baka <baka@mpedigree.net>
2016-01-17 21:29:42 +0000

    Ben: pass page title  to form inclusion

--- contacts/templatetags/form.py
+++ contacts/templatetags/form.py
@@ -3,7 +3,8 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('contacts/contact_form.html')
-def contact_form(form, contact, categories):
+def contact_form(form, contact, categories, title):
     return {'form':form,
             'categories': categories,
+            'title': title,
             'url': "/contacts/%s/update"%contact.id}
