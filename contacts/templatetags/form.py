da0db56014d3657e691a374c5d9cd9d50ca07da2
Benjamin Baka <baka@mpedigree.net>
2016-01-17 20:50:26 +0000

    Ben: conduit between actual html file rendered and form

--- contacts/templatetags/form.py
+++ contacts/templatetags/form.py
@@ -1,1 +1,9 @@
-__author__ = 'ben'
+from django import template
+
+register = template.Library()
+
+@register.inclusion_tag('contacts/contact_form.html')
+def contact_form(form, contact, categories):
+    return {'form':form,
+            'categories': categories,
+            'url': "/contacts/%s/update"%contact.id}
