8dd570ac7eb38d67a5bb16f85e10080e01ae2050
eugene <ugence@gmail.com>
2017-07-13 13:29:04 +0100

    removed userprofile from admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -4,5 +4,4 @@ from contacts.models.user_profile import UserProfile
 from contacts.models.contact import Contact
 # Register your models here.
 admin.site.register(Category)
-admin.site.register(UserProfile)
 admin.site.register(Contact)
